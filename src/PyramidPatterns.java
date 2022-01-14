2f47d2b0cd8f74ff75cf7c52c6ff79f2d6ffc789
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-01-14 23:57:12 +0600

    ⭐Added Inverted Half Pyramid Pattern

--- src/PyramidPatterns.java
+++ src/PyramidPatterns.java
@@ -1,17 +1,31 @@
 public class PyramidPatterns {
     public static void main(String[] args) {
+        int length = 6;
 
         System.out.println("""
-                            +--------------+
-                            | Half Pyramid |
-                            +--------------+
-                            """);
-        halfPyramid(5);
+                            +----------------------+
+                            |     Half Pyramid     |
+                            +----------------------+""");
+        halfPyramid(length);
+
+        System.out.println("""
+                            +-----------------------+
+                            | Inverted Half Pyramid |
+                            +-----------------------+""");
+        invertedHalfPyramid(length);
 
     }
 
     public static void halfPyramid(int length) {
         for(int i=1; i<=length; i++){
+            for (int j=1; j<=i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+
+    public static void invertedHalfPyramid(int length) {
+        for(int i=length; i>=1; i--){
             for (int j=1; j<=i; j++)
                 System.out.print("* ");
             System.out.println();
